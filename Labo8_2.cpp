2958f59e2aec90db4e7200dca5d093f6b276d99a
zoozie <topjada2@gmail.com>
2025-11-17 23:03:15 +0200

    sorting values by vectors

--- Labo8_2.cpp
+++ Labo8_2.cpp
@@ -9,14 +9,34 @@ int main() {
 
 	std::vector <float> a;
 
+	//sort numbers into 3 vectors
+	struct SortElements {
+		std::vector <float> negative;
+		std::vector <float> zero;
+		std::vector <float> positive;
+	};
+
+	SortElements sorted_numbers;
+
 	printf("How many numbers massive needs? Enter amount: ");
 	scanf("%i", &quantity);
 
-	for (i = 0, i <= quantity - 1; i++) {
-		printf("enter value of massive number %i", i);
+	for (i = 0; i <= quantity - 1; i++) {
+		printf("enter value of a[%i]=", i);
 		scanf("%f", &enter_value);
 
 		a.push_back(enter_value);
+		
+		//putting values into vectors
+	if (a[i] < 0) {
+		sorted_numbers.negative.push_back(a[i]);
+		}
+	else if (a[i] == 0) {
+		sorted_numbers.zero.push_back(a[i]);
+		}
+	else {
+		sorted_numbers.positive.push_back(a[i]);
+		}
 	}
 
 	return 0;
